3056e5636772223cfafab2fd8fb834c23524ed6b
Will Forbes <wforbes87@gmail.com>
2023-01-15 00:36:29 -0800

    Pictures - try using single useState hook with object

--- pages/pictures/index.js
+++ pages/pictures/index.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import styles from '../../styles/Pictures.module.css'
 
 export const getStaticProps = async () => {
-	const res = await fetch('https://jsonplaceholder.typicode.com/photos')
+	const res = await fetch('https://jsonplaceholder.typicode.com/photos?_')
 	const data = await res.json()
 
 	return {
@@ -16,30 +16,43 @@ export const getStaticProps = async () => {
 const Projects = ({ photos }) => {
 	const maxPerPage = 16;
 	const pages = Math.ceil(photos.length / maxPerPage);
-	let [displayPhotos, setDisplayPhotos] = useState([...photos.slice(0, maxPerPage)])
-	let [currentPage, setCurrentPage] = useState(1)
-	const incrementPage = () => setCurrentPage(() => (currentPage+1))
-	const decrementPage = () => setCurrentPage(() => (currentPage-1))
+	let [state, setState] = useState({
+		currentPage: 1,
+		displayPhotos: [...photos.slice(0, maxPerPage)]
+	})
 	
 	const navPrevPage = () => {
-		if (currentPage === 1) return;
-		decrementPage()
-		let newPage = currentPage - 1 //hook won't update in time, get new page number
-		// get display photo set with one less than new page number to account for zeroth element offset
-		setDisplayPhotos(() => ([...photos.slice((newPage-1)*maxPerPage, (newPage-1)*maxPerPage+maxPerPage)]))
+		if (state.currentPage === 1) return
+		setState(prevState => ({
+			currentPage: prevState.currentPage - 1,
+			displayPhotos: [
+				// use currentPage-2 multipler to account for zero index offset
+				...photos.slice(
+					(prevState.currentPage-2)*maxPerPage,
+					(prevState.currentPage-2)*maxPerPage+maxPerPage
+				)
+			]
+		}))
 	}
 
 	const navNextPage = () => {
-		if (currentPage === pages) return;
-		incrementPage()
-		setDisplayPhotos([...photos.slice(currentPage*maxPerPage, currentPage*maxPerPage+maxPerPage)])
+		if (state.currentPage === pages) return
+		setState(prevState => ({
+			currentPage: prevState.currentPage + 1,
+			displayPhotos: [
+				...photos.slice(
+					(prevState.currentPage+1)*maxPerPage,
+					(prevState.currentPage+1)*maxPerPage+maxPerPage
+				)
+			]
+		}))
 	}
 
 	return (
 		<div>
 			<h1>Pictures</h1>
 			<div className={styles.thumbContainer}>
-				{displayPhotos.map(photo => (
+				{state.displayPhotos.map(photo => (
 					<div className={styles.thumb} key={`photo-${photo.id}`}>
 						<Image src={photo.url + '.png'} width={100} height={100} alt={photo.title} priority={photo.id===1} />
 						<div>Pic #{photo.id}</div>
@@ -48,7 +61,7 @@ const Projects = ({ photos }) => {
 			</div>
 			<div className={styles.container}>
 				<div className={styles.btn} onClick={navPrevPage}>Prev</div>
-				<div>Page {currentPage} of {pages}</div>
+				<div>Page {state.currentPage} of {pages}</div>
 				<div className={styles.btn} onClick={navNextPage}>Next</div>
 			</div>
 		</div>
