5fb1236befa2f00adb1da6be864bc907c02f565f
Will Forbes <wforbes87@gmail.com>
2023-01-15 01:54:01 -0800

    Pictures - paginate requests, add placeholders
    - pictures are now fetched 16 at a time, per page upon prev/next click with the total pictures gathered from a seperate call from static props
    - add placeholder pictures that show while page of pics is being loaded

--- pages/pictures/index.js
+++ pages/pictures/index.js
@@ -1,50 +1,74 @@
 import Image from 'next/image'
 import { useState } from 'react'
 import styles from '../../styles/Pictures.module.css'
+import PhotoPlaceholders from './PhotoPlaceholders'
 
+const initialMaxPerPage = 16
 export const getStaticProps = async () => {
-	const res = await fetch('https://jsonplaceholder.typicode.com/photos?_')
-	const data = await res.json()
+	const photoRes = await fetch(`https://jsonplaceholder.typicode.com/photos?_start=0&_limit=${initialMaxPerPage}`)
+	const photoData = await photoRes.json()
+
+	const totalRes = await fetch(`https://jsonplaceholder.typicode.com/photos?_start=0&_end=1`)
+	let totalCount = totalRes.headers.get('x-total-count');
+	if(typeof totalCount !== 'undefined' && totalCount.length > 0) {
+		totalCount = Number.parseInt(totalCount)
+	}
+
 
 	return {
 		props: {
-			photos: data
+			photos: photoData,
+			total: totalCount
 		}
 	}
 }
 
-const Projects = ({ photos }) => {
-	const maxPerPage = 16;
-	const pages = Math.ceil(photos.length / maxPerPage);
+const Projects = ({ photos, total }) => {
 	let [state, setState] = useState({
+		loading: false,
+		maxPerPage: initialMaxPerPage,
+		pages: Math.ceil(total / initialMaxPerPage),
 		currentPage: 1,
-		displayPhotos: [...photos.slice(0, maxPerPage)]
+		displayPhotos: [...photos.slice(0, initialMaxPerPage)]
 	})
-	
-	const navPrevPage = () => {
-		if (state.currentPage === 1) return
+
+	const fetchPhotos = async (page) => {
+		if (page > state.pages) return
+		if (page < 0) page = 0
+		
 		setState(prevState => ({
-			currentPage: prevState.currentPage - 1,
-			displayPhotos: [
-				// use currentPage-2 multipler to account for zero index offset
-				...photos.slice(
-					(prevState.currentPage-2)*maxPerPage,
-					(prevState.currentPage-2)*maxPerPage+maxPerPage
-				)
-			]
+			...prevState,
+			loading: true
+		}))
+		
+		const res = await fetch(`https://jsonplaceholder.typicode.com/photos?_start=${page*state.maxPerPage}&_limit=${state.maxPerPage}`)
+		const photos = await res.json()
+		
+		setState(prevState => ({
+			...prevState,
+			loading: false
+		}))
+		return photos
+	}
+	
+	const navNextPage = async () => {
+		if (state.currentPage === state.pages) return
+		console.log(state.currentPage)
+		const photos = await fetchPhotos(state.currentPage + 1)
+		setState(prevState => ({
+			...prevState,
+			currentPage: prevState.currentPage + 1,
+			displayPhotos: photos
 		}))
 	}
 
-	const navNextPage = () => {
-		if (state.currentPage === pages) return
+	const navPrevPage = async () => {
+		if (state.currentPage === 1) return
+		const photos = await fetchPhotos(state.currentPage - 2)
 		setState(prevState => ({
-			currentPage: prevState.currentPage + 1,
-			displayPhotos: [
-				...photos.slice(
-					(prevState.currentPage+1)*maxPerPage,
-					(prevState.currentPage+1)*maxPerPage+maxPerPage
-				)
-			]
+			...prevState,
+			currentPage: prevState.currentPage - 1,
+			displayPhotos: photos
 		}))
 	}
 
@@ -52,17 +76,28 @@ const Projects = ({ photos }) => {
 		<div>
 			<h1>Pictures</h1>
 			<div className={styles.thumbContainer}>
-				{state.displayPhotos.map(photo => (
-					<div className={styles.thumb} key={`photo-${photo.id}`}>
-						<Image src={photo.url + '.png'} width={100} height={100} alt={photo.title} priority={photo.id===1} />
-						<div>Pic #{photo.id}</div>
-					</div>
-				))}
+				{state.loading ?
+					<PhotoPlaceholders maxPageTotal={state.maxPerPage} />
+					:
+					state.displayPhotos ? state.displayPhotos.map(photo => (
+						<div className={styles.thumb} key={`photo-${photo.id}`}>
+							<Image
+								src={photo.url + '.png'}
+								width={100}
+								height={100}
+								alt={photo.title}
+								priority={photo.id===1}
+								className={styles.photo}
+							/>
+							<div>Pic #{photo.id}</div>
+						</div>
+					)) : 'There was an error! Stop drop and roll immediately.'
+				}
 			</div>
 			<div className={styles.container}>
-				<div className={styles.btn} onClick={navPrevPage}>Prev</div>
-				<div>Page {state.currentPage} of {pages}</div>
-				<div className={styles.btn} onClick={navNextPage}>Next</div>
+				<button className={[styles.btn, ].join(' ')} onClick={navPrevPage}>Prev</button>
+				<div>Page {state.currentPage} of {state.pages}</div>
+				<button className={styles.btn} onClick={navNextPage}>Next</button>
 			</div>
 		</div>
 	)
