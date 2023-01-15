bfbdde547a3d3755a2222cab670935b0545f6092
Will Forbes <wforbes87@gmail.com>
2023-01-14 18:02:36 -0800

    Add redirect and countdown to 404 page
    - add useEffect and useState hooks to 404 page
    - add useEffect call for a countdown timer that redirects after a specific number of seconds
    - use useState hook to handle state of redirect countdown and display the seconds on the page

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,27 @@
 import Link from 'next/Link'
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 
 const NotFound = () => {
+	const router = useRouter()
+	let [seconds, setSeconds] = useState(3)
+
+	useEffect(() => {
+		setTimeout(() => {
+			if (seconds <= 0) {
+				router.push('/')
+				return
+			}
+			setSeconds(seconds - 1)
+		}, 1000)
+	}, [seconds])
+
 	return (
 		<div className="not-found">
 			<h1>Oops!</h1>
 			<h2>That page cannot be found.</h2>
-			<p>Go back to the <Link href="/">Home Page</Link>.</p>
+			<p>Redirecting you to the Home Page in {seconds} seconds...</p>
+			<p>Or you can click here: <Link href="/">Home Page</Link>.</p>
 		</div>
 	);
 }
