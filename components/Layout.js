eac18f3c135d382782a009e0a6076ce4cc2a128c
Will Forbes <wforbes87@gmail.com>
2023-01-14 22:12:28 -0800

    add head title with pathname to layout
    - use nextjs head component to add title on the layout
    - use next router to get the path name of the current page, displayed in the title on layout with some formatting

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,30 @@
+import Head from "next/head"
+import { useRouter } from 'next/router'
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 
 const Layout = ({ children }) => {
+	
+	const router = useRouter()
+
+	const getPathName = () => {
+		let displayPath = router.pathname.substring(1, router.pathname.length)
+		if (displayPath.length === 0) return "Home"
+		return displayPath[0].toLocaleUpperCase() + displayPath.substring(1, displayPath.length)
+	}
+
 	return (
-		<div className="content">
-			<Navbar />
-			{ children }
-			<Footer />
-		</div>
-	);
+		<>
+			<Head>
+				<title>wf-nextjs-practice | {getPathName()}</title>
+			</Head>
+			<div className="content">
+				<Navbar />
+				{ children }
+				<Footer />
+			</div>
+		</>
+	)
 }
  
-export default Layout;
+export default Layout
