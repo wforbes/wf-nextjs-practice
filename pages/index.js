562f9f99eaad63ba1895bcb748ce4015daef8258
Will Forbes <wforbes87@gmail.com>
2023-01-25 22:46:24 -0800

    fix pictures link path on home page

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ const Home = () => {
 			<h1 className={styles.title}>Home</h1>
 			<p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ipsa aspernatur rerum ducimus assumenda deleniti rem expedita! Incidunt nesciunt sequi repellat asperiores nostrum quae minus adipisci, voluptas, alias voluptates eum?</p>
 			<p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ipsa aspernatur rerum ducimus assumenda deleniti rem expedita! Incidunt nesciunt sequi repellat asperiores nostrum quae minus adipisci, voluptas, alias voluptates eum?</p>
-			<Link href="/projects" className={styles.btn}>
-				See Projects listing
+			<Link href="/pictures" className={styles.btn}>
+				See Pictures listing
 			</Link>
 		</div>
 	)
