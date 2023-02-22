b9345267f3c2fb68f1191af9306aa6fb48f0c4c0
Will Forbes <wforbes87@gmail.com>
2023-02-22 12:05:41 -0800

    clean up files from failed db attempt
    - remove files from an attempt to run the database api on this project, that's been moved to wf-nodejs-practice project

--- pages/api/dbTest.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { setup } from './infrastructure'
-
-export default async function handler (req, res) {
-	await setup()
-	
-	res.status(200).json({ mongoStatus: true })
-}
